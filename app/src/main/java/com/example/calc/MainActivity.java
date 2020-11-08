d2658a0b2dbb4d80d7ead92ea52ec351bc927a5c
ganes <gustiganes@gmail.com>
2020-11-08 20:53:28 +0800

    style number calc

--- app/src/main/java/com/example/calc/MainActivity.java
+++ app/src/main/java/com/example/calc/MainActivity.java
@@ -3,13 +3,12 @@ package com.example.calc;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.content.res.ColorStateList;
+
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.CompoundButton;
 import android.widget.Switch;
-import android.widget.ToggleButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,10 +26,19 @@ public class MainActivity extends AppCompatActivity {
         switchButton = (Switch) findViewById(R.id.switch1);
         boolean darkMode = switchButton.isChecked();
         if(darkMode){
+            // switch Button
             switchButton.setText("Dark");
+            switchButton.setBackgroundColor(Color.parseColor("#000000"));
+            switchButton.setTextColor(Color.parseColor("#FFFFFF"));
+
+            // constraint layout
             constlay.setBackgroundColor(Color.parseColor("#000000"));
         }else{
+            // switch Button
             switchButton.setText("Light");
+            switchButton.setTextColor(Color.parseColor("#000000"));
+            switchButton.setBackgroundColor(Color.parseColor("#FFFFFF"));
+            // constraint layout
             constlay.setBackgroundColor(Color.parseColor("#FFFFFF"));
         }
     }
