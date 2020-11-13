f167a99d5efc7fb19b466f1b9cf2243a17bb2d37
ganes <gustiganes@gmail.com>
2020-11-14 00:47:02 +0800

    displayed number

--- app/src/main/java/com/example/calc/MainActivity.java
+++ app/src/main/java/com/example/calc/MainActivity.java
@@ -7,13 +7,22 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.HorizontalScrollView;
 import android.widget.Switch;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener, View.OnClickListener{
+    // variable used in many function
     private  ConstraintLayout constlay;
     private Switch switchButton;
+    private TextView textView;
+
     // Switch control
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -39,14 +48,51 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
     }
 
     @Override
+    public void onClick(View v) {
+        textView = findViewById(R.id.textView);
+//        for (int i = 0; i < 10;) {
+//            int id = getResources().getIdentifier("button"+i, "id", getPackageName());
+//            Button button = findViewById(id);
+//            textView.setText(button.getText());
+//            i++;
+//        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         constlay = findViewById(R.id.constLay);
         switchButton = findViewById(R.id.switch1);
-
         switchButton.setOnCheckedChangeListener(this);
+
+    }
+    public void onClickNumbering(View v){
+        textView = findViewById(R.id.textView);
+        String displayNumber = textView.getText().toString();
+        // force scroll to right
+        HorizontalScrollView hrw = findViewById(R.id.hzw);
+        hrw.fullScroll(View.FOCUS_RIGHT);
+        // numbering button had a same behavior
+        for (int i = 0; i < 10;) {
+            int id = getResources().getIdentifier("button"+i, "id", getPackageName());
+            if(v.getId() == id){
+                textView.setText(displayNumber + i);
+                break;
+            }
+            if(v.getId() == R.id.buttonComma){
+                textView.setText(displayNumber + ".");
+                break;
+            }
+            i++;
+        }
+    }
+    // next build click for all operator adding
+    public void onClickOperator(View v){
+
     }
 
+
 }
+
